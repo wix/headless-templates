d448042f60a86deaad18836796c714020ebcd112
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-02-04 12:36:14 +0200

    build: integrate astro-ricos and astro-blog-loader (#5)
    * remove redundant
    
    * install deps

--- packages/astro-blog-loader/index.ts
+++
@@ -1,4 +0,0 @@
-import { wixBlogLoader } from "./src/loader";
-import { wixBlogLoaderSchema } from "./src/schema";
-
-export { wixBlogLoader, wixBlogLoaderSchema };
--- packages/astro-blog-loader/src/schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "astro/zod";
-
-export const wixBlogLoaderSchema = z.object({
-  id: z.string(),
-  data: z.any(), // TODO: infer the schema from wix sdk
-});
