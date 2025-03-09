41e39dec16ffa3272309670f645fb255cd61ff47
nitayneeman <nitayneeman@users.noreply.github.com>
2025-03-09 13:17:23 +0200

    add wix adapter

--- astro/blog/astro.config.mjs
+++ astro/blog/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import wix from "@wix/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [sitemap()],
+  adapter: wix(),
 });
