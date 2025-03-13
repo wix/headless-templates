ceac6d66923852edb7c960dc9c3e751647d1aa11
Netanel Gilad <netanelg@wix.com>
2025-03-13 11:23:45 +0200

    add tailwind bootstrap to blank template

--- astro/blank/astro.config.mjs
+++ astro/blank/astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import wix from "@wix/astro";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    adapter: wix()
+  adapter: wix(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
