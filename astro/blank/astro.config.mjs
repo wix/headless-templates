dce481dc1bd2a778e09a7fb2a51bcb398f800039
Netanel Gilad <netanelg@wix.com>
2025-03-14 16:03:52 +0200

    Revert "add react support to blank astro template"
    This reverts commit bd4107a22978f7da68adafab88407b159a05b917.

--- astro/blank/astro.config.mjs
+++ astro/blank/astro.config.mjs
@@ -4,15 +4,11 @@ import wix from "@wix/astro";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   adapter: wix(),
 
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  }
 });
