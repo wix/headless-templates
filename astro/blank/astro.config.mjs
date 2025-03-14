bd4107a22978f7da68adafab88407b159a05b917
Netanel Gilad <netanelg@wix.com>
2025-03-14 15:02:13 +0200

    add react support to blank astro template

--- astro/blank/astro.config.mjs
+++ astro/blank/astro.config.mjs
@@ -4,11 +4,15 @@ import wix from "@wix/astro";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: wix(),
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
