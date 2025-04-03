6925a9a4a01f69ea3c166ab0a8ba297eff63cb02
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-04-03 08:50:49 +0300

    implement template

--- astro/scheduler/astro.config.mjs
+++ astro/scheduler/astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import wix from "@wix/astro";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   adapter: wix(),
 });
