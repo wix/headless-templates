bd10e8bb361199ca9efd95ab274d2fddd856fa6b
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-04-07 08:34:24 +0300

    temp fix

--- astro/scheduler/astro.config.mjs
+++ astro/scheduler/astro.config.mjs
@@ -14,4 +14,11 @@ export default defineConfig({
     }),
   ],
   adapter: wix(),
+  vite: {
+    resolve: {
+      alias: {
+        jose: "../../node_modules/jose/dist/browser/index.js",
+      },
+    },
+  },
 });
