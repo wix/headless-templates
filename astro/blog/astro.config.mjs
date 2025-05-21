ef5f4bab75252cebcb7459e9137fe46fb3329c53
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-05-21 09:57:46 +0300

    use ricos viewer

--- astro/blog/astro.config.mjs
+++ astro/blog/astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import wix from "@wix/astro";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [wix(), sitemap()]
+  integrations: [wix(), sitemap(), react()],
 });
