80451d0ed7a4a0ea9aaf4099e27ad5b3bc7d3106
Dor Chaouat <82763443+dor-chaouat@users.noreply.github.com>
2025-05-25 10:29:33 +0300

    migration: astro integration - tailwind v4 - cleanup (#280)

--- astro/commerce/astro.config.mjs
+++ astro/commerce/astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import wix from "@wix/astro";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
-  adapter: wix(),
+  integrations: [wix()],
   image: {
     domains: ["static.wixstatic.com"],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
