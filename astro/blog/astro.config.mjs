634210ec3942a3e2a39b2d3da572309dc1347d76
guyofeck <guyo@wix.com>
2026-03-08 08:53:13 +0200

    Add @wix/astro-pages integration to all Astro templates (#665)
    * Add @wix/astro-pages integration to all Astro templates

--- astro/blog/astro.config.mjs
+++ astro/blog/astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import wix from "@wix/astro";
+import wixPages from "@wix/astro-pages";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [wix(), sitemap()]
+  integrations: [wix(), wixPages(), sitemap()]
 });
