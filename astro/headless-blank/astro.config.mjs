308beca2890f757a2201bb50020de3bfdd81e35e
omri maman <omrim@wix.com>
2026-08-02 11:11:06 +0300

    feat: migrate Astro templates to @wix/astro-wix-hosting-adapter (#686)

--- astro/headless-blank/astro.config.mjs
+++ astro/headless-blank/astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import wix from "@wix/astro";
 import wixPages from "@wix/astro-pages";
 import react from "@astrojs/react";
-import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
-
-const isBuild = process.env.NODE_ENV == "production";
+import wixHostingAdapter from "@wix/astro-wix-hosting-adapter";
 
 export default defineConfig({
   output: "server",
+  adapter: wixHostingAdapter(),
   integrations: [wix(), wixPages(), react()],
   security: { checkOrigin: false },
   image: { domains: ["static.wixstatic.com"] },
-  ...(isBuild && { adapter: cloudProviderFetchAdapter({}) }),
 });
